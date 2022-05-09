8b2f71577d13b72f646656c5524bb6cc73a8590a
Szymon <golus.szymon@gmail.com>
2022-05-09 21:47:27 +0200

    Better looking client form, working links in menu

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+import re
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('menu.html')
+
+
+@app.route('/client_add', methods=['POST', 'GET'])
+def client_add():
+    if request.method == 'GET':
+        # Client-add form
+        return render_template('client_add.html', client_add=client_add)
+    else:
+        body = request.form['firm']
+
+        return body
+        # Client added information
